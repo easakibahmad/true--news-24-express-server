eb94a72800dc2838f06f5fb2ee82516fc7fc8205
easakibahmad <sakib.sgc.du@gmail.com>
2022-10-23 15:02:36 +0600

    news json file added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 4000
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.use(cors())
 
@@ -13,6 +14,28 @@ app.get('/', (req,res) => {
 app.get('/news-categories', (req, res) =>{
     res.send(categories)
 })
+
+
+app.get('/categories/:id',(req, res)=>{
+    const id = req.params.id
+    if(id === 08){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews)
+    }
+})
+
+
+app.get('/news/:id', (req, res)=>{
+    // console.log(req.params.id)
+    const id = req.params.id
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+
 
 app.listen(port, ()=> {
     console.log(`news server is running on port ${port}`)
